53966dc57a60193f783f63fca2e8cd0a2b38e289
Ronyt21 <ronitonwork24x7@gmail.com>
2023-09-30 09:36:12 +0530

    Comparison in javascript

--- 0_Basics/1_conversion.js
+++ 0_Basics/1_conversion.js
@@ -1,6 +1,15 @@
+//***************************************  Conversion  *******************************************
+
 // let score = 33;
 // console.log(typeof score);
 // score = String(score);
 // console.log(typeof score);
 
 
+//***************************************  Operations  *******************************************
+console.log("2"+1);  // 21
+console.log(1+"2");  // 12
+console.log("2"+1+1); // 211
+console.log(2+1+'1');// 31
+console.log(+false); //0
+// Readability is most impostant in coding 
---
+++ 0_Basics/2_Comparisons.js
@@ -0,0 +1,11 @@
+// *************************************************** Comparison *************************************************************
+// console.log("2">1);
+// console.log("02">1);
+// Typescript only gives you some strict rule to something in ethical way
+// Make sure you have same datatype before comparing two conditionals 
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+// Comparison and Equality check works in diffrent way 
+// avoid confusing code 
