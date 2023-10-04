11395613c1ab7861ba4433a8e4b2b41ace295d91
Ronyt21 <ronitonwork24x7@gmail.com>
2023-10-04 09:31:33 +0530

    datatypes

---
+++ 0_Basics/3_Datatypes.js
@@ -0,0 +1,70 @@
+// **************************************************** Datatypes **********************************************************
+// Data types are dividied into two official types on the basis of how memory keep them and how we access them
+// 1. Primitive 
+    //   String , Boolean , Number , null , undefined , symbol ( use to make any value unique ), BigInt
+      
+ 
+// 2. Non-Primitive (Reference type)
+// Array , Objects , Functions 
+
+// **If you want to master javascript then work on objects and browser methods
+
+// const id = Symbol('123');
+// const anotherID = Symbol('123');
+
+// console.log(anotherID===id);
+// console.log(typeof(id));
+
+// ******************************************************** Practice ****************************************************************
+
+ // String 
+//  const str = 'hello world';
+//  console.log(typeof(str)); // string 
+
+
+//  // Boolean 
+//  const OnorOff = true;
+//  console.log(typeof(OnorOff)); // boolean
+
+
+//  // Number
+//  const number = 23;
+//  console.log(typeof(number)); //number 
+
+
+//  // null 
+//  const ACtemp = null;
+//  console.log(typeof(ACtemp)); // object
+
+
+//  // undefined
+//  const userEmail = undefined;
+//  console.log(typeof(userEmail)); // undefined
+
+
+//  // symbol 
+//  const userID = Symbol('234');
+//  console.log(typeof(userID)); // symbol
+
+
+//  // BigInt
+//  const bigNumber = 223333355556666666677777777777;
+//  console.log(typeof(bigNumber)); // number 
+
+
+//  // Array
+//  const arr = ['ronit', 'raj', 'orange', 'apple'];
+//  console.log(typeof(arr)); //object
+
+
+//  // Objects 
+//  const superheroes = {
+//      name:'iron man',
+//      strength:'iron suit'}
+//  console.log(typeof(superheroes)); //object
+    
+
+//  // Functions 
+//  const fun = ()=>{
+//      console.log('i am a function');};
+//  console.log(typeof(fun)); //function
